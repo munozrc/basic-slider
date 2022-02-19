3c1b3ca3438da9e98e4bffa22dc1c02357b44c32
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-02-18 22:49:33 -0500

    feat: Set functions for slide move

--- src/lib/slider.js
+++ src/lib/slider.js
@@ -4,9 +4,11 @@ export default function Slider(container) {
 
   const _container = document.querySelector(`${container}`)
   const _slides = _container?.querySelectorAll('.slider__item')
+  const _btnPrev = _container?.querySelector('.slider__btn-prev')
+  const _btnNext = _container?.querySelector('.slider__btn-next')
 
   let _dots = [] // Array Nodes
-  let _activeSlide = 1
+  let _activeSlide = 0
 
   if (typeof container === 'undefined' || container === '') 
     throw Error('It is necessary to pass the identifier of the slider as an argument. Examples: "#slider" or "div.slider"')
@@ -17,30 +19,71 @@ export default function Slider(container) {
   if (_slides.length === 0) 
     throw Error(`Elements with class ".slider__item" must exist inside the "${container}" container.`)
 
+  const sectionWrapper = document.createElement('section')
+  const sectionContent = document.createElement('section')
+
+  sectionWrapper.className = 'slider__wrapper'
+  sectionContent.className = 'slider__content'
+
+  sectionContent.append(..._slides)
+  sectionWrapper.append(...[_btnPrev, sectionContent, _btnNext])
+
+  _container.innerHTML = ''
+  _container.appendChild(sectionWrapper)
 
   const navContainer = document.createElement('nav')
   const navElements = document.createElement('ul')
   
-  navElements.className = 'slider-dots'
-  navContainer.className = 'slider-nav'
+  navContainer.className = 'slider__nav'
+  navElements.className = 'slider__dots'
 
   navContainer.appendChild(navElements)
   _container.appendChild(navContainer)
 
   _slides.forEach((slide, index) => {
-    const dataIndex = index + 1
     const dotElement = document.createElement('button')
     
-    dotElement.className = 'slider-dot'
-    dotElement.dataset.index = dataIndex
-    navElements.appendChild(dotElement)
+    dotElement.className = index === 0 ? 'slider__dot active' : 'slider__dot'
+    dotElement.dataset.index = index
 
-    slide.dataset.index = dataIndex
+    navElements.appendChild(dotElement)
+    slide.dataset.index = index
   })
 
-  _dots = _container?.querySelectorAll('nav > ul > .slider-dot')
+  _dots = _container?.querySelectorAll('nav > ul > .slider__dot')
+
+  _btnPrev.addEventListener('click', () => {
+    if (_activeSlide >= 0) _activeSlide--
+    slideTo(sectionContent, _activeSlide)
+    setActiveDot(_dots, _activeSlide)
+    console.log({ _activeSlide })
+  })
+  
+  _btnNext.addEventListener('click', () => {
+    if (_activeSlide <= _slides.length) _activeSlide++
+    slideTo(sectionContent, _activeSlide)
+    setActiveDot(_dots, _activeSlide)
+    console.log({ _activeSlide })
+  })
 
   return {
     values: `Slides ${_slides.length}`
   }
+}
+
+function slideTo(content, index) {
+  content.scrollTo({
+    left: index * content.offsetWidth
+  })
+}
+
+// function getSlideSelect(arraySlides, index) {
+//   return arraySlides.findIndex(item => item.dataset.index === index)
+// }
+
+function setActiveDot(arrayDots, index) {
+  arrayDots.forEach(dot => {
+    if (+dot.dataset.index === index) dot.classList.add('active')
+    else dot.classList.remove('active')
+  })
 }
